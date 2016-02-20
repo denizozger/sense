2aee7e2be1b2035a37b1d077f14a832209bb9dae
Deniz Ozger <denizozger@gmail.com>
2016-02-20 11:22:57 +0000

    better logging

--- send_readings.py
+++ send_readings.py
@@ -1,15 +1,18 @@
 import requests
-import time
 import os
+import time
+from time import gmtime, strftime
 
 from sense_hat import SenseHat
 
-url = os.getenv('HOME_API_URL', 'http://localhost:5000/readings') + '/readings'
+url = os.getenv('HOME_API_URL', 'http://localhost:5000') + '/readings'
 readings = dict()
 response = None
 sense = SenseHat()
 
-while True: 
+print('URL set to', url)
+
+while True:
 	temperature = round(sense.get_temperature(), 1)
 	pressure = round(sense.get_pressure(), 1)
 	humidity = round(sense.get_humidity(), 1)
@@ -20,8 +23,6 @@ while True:
 					time=int(time.time()))
 
 	response = requests.post(url, data=readings, allow_redirects=True)
-	print(response.content, readings)
+	print(response.status_code, strftime('%d-%m-%Y %H:%M:%S', gmtime()), readings)
 
 	time.sleep(300)
-
-

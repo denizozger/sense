c67c9d23bf67f5f58c2fc243cd5edad7bd826898
Deniz Ozger <denizozger@gmail.com>
2016-02-21 09:47:16 +0000

    Removed infinite loop in favour of cron jobs

--- send_readings.py
+++ send_readings.py
@@ -13,17 +13,14 @@ sense = SenseHat()
 
 log.info('URL set to %s', url)
 
-while True:
-	temperature = round(sense.get_temperature(), 1)
-	pressure = round(sense.get_pressure(), 1)
-	humidity = round(sense.get_humidity(), 1)
+temperature = round(sense.get_temperature(), 1)
+pressure = round(sense.get_pressure(), 1)
+humidity = round(sense.get_humidity(), 1)
 
-	readings = dict(temperature=temperature,
-					pressure=pressure,
-					humidity=humidity,
-					time=int(time.time()))
+readings = dict(temperature=temperature,
+				pressure=pressure,
+				humidity=humidity,
+				time=int(time.time()))
 
-	response = requests.post(url, data=readings, allow_redirects=True)
-	log.info('%s %s %s', response.status_code, strftime('%d-%m-%Y %H:%M:%S', gmtime()), readings)
-
-	time.sleep(300)
+response = requests.post(url, data=readings, allow_redirects=True)
+log.info('%s %s %s', response.status_code, strftime('%d-%m-%Y %H:%M:%S', gmtime()), readings)
